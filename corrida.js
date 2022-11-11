2cba3a1a677b881dcf3c56f55e408c9c5b50d69e
Diego Alves <diegoteialves@gmail.com>
2022-11-10 21:30:52 -0300

    Número 2 e 3

--- corrida.js
+++ corrida.js
@@ -4,21 +4,27 @@
 ● Potência - número de cavalos de potência do carro;
 ● Velocidade Máxima - qual a maior velocidade que o carro pode alcançar;
 ● Aceleração - o tempo em segundos que o carro leva para ir de 0 a 100 km/h
+
 2. Adicione na classe carro um método que recebe como parâmetro uma distância em
 metros e calcula o tempo em segundos para percorrer essa distância. Use a
 seguinte fórmula para o cálculo: resultado = distância / (Velocidade Máxima /
 Aceleração). A função deve retornar esse resultado.
+
 3. Crie alguns objetos da classe Carro e adicione-os em um array
+
 4. Crie uma classe Corrida com as seguintes propriedades:
 ● Nome - nome do local da corrida;
 ● Tipo - uma corrida pode ser Fórmula 1, Stock Car, Rally, etc;
 ● Distância - o total em metros da corrida;
 ● Vencedor - qual a equipe que ganhou a corrida;
+
 5. Adicione na classe Corrida um método que verifica qual foi o carro que completou a
 corrida em menor tempo, para isso utilize o método criado na classe carro. Salve o
 nome da equipe que fez o menor tempo na propriedade “Vencedor”.
+
 6. Adicione na classe Corrida um método que exibe na tela quem é o vencedor da
 corrida.
+
 7. Crie um objeto da classe Corrida e chame seus métodos.
 */
 
@@ -28,4 +34,25 @@ class Carro{
     VelocidadeMaxima
     Aceleracao
 
+    MetrosSegundos(){
+        let resultado = distancia / (this.VelocidadeMaxima / this.Aceleracao) 
+        return resultado;
+    }
 }
+
+let carro = new Carro()
+let NomeEquipes = []
+let Potencias = []
+let VelocidadeMaximas = []
+let Aceleracaos = []
+let indexGeral = 0
+
+carro.NomeEquipe = "ferrari"
+carro.Potencia = 450
+carro.VelocidadeMaxima = 390
+carro.Aceleracao = 6
+
+NomeEquipes[indexGeral] = carro.NomeEquipe
+Potencias[indexGeral] = carro.Potencia
+VelocidadeMaximas[indexGeral] = carro.VelocidadeMaxima
+Aceleracaos[indexGeral] = carro.Aceleracao
