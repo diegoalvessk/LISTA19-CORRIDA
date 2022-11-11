29e953dd9a4031cc44a94a7a1e7e689545c5443b
Diego Alves <diegoteialves@gmail.com>
2022-11-10 21:23:58 -0300

    Ciração da classe carro

---
+++ corrida.js
@@ -0,0 +1,31 @@
+/*
+1. Crie uma classe Carro com as seguintes propriedades:
+● Nome - qual a equipe do carro Ferrari, Mercedes, McLaren, etc;
+● Potência - número de cavalos de potência do carro;
+● Velocidade Máxima - qual a maior velocidade que o carro pode alcançar;
+● Aceleração - o tempo em segundos que o carro leva para ir de 0 a 100 km/h
+2. Adicione na classe carro um método que recebe como parâmetro uma distância em
+metros e calcula o tempo em segundos para percorrer essa distância. Use a
+seguinte fórmula para o cálculo: resultado = distância / (Velocidade Máxima /
+Aceleração). A função deve retornar esse resultado.
+3. Crie alguns objetos da classe Carro e adicione-os em um array
+4. Crie uma classe Corrida com as seguintes propriedades:
+● Nome - nome do local da corrida;
+● Tipo - uma corrida pode ser Fórmula 1, Stock Car, Rally, etc;
+● Distância - o total em metros da corrida;
+● Vencedor - qual a equipe que ganhou a corrida;
+5. Adicione na classe Corrida um método que verifica qual foi o carro que completou a
+corrida em menor tempo, para isso utilize o método criado na classe carro. Salve o
+nome da equipe que fez o menor tempo na propriedade “Vencedor”.
+6. Adicione na classe Corrida um método que exibe na tela quem é o vencedor da
+corrida.
+7. Crie um objeto da classe Corrida e chame seus métodos.
+*/
+
+class Carro{
+    NomeEquipe
+    Potencia
+    VelocidadeMaxima
+    Aceleracao
+
+}
